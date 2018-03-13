70f4b54f2a3c3ada581cb4c35c1c5dad30ed213a
perrym <perrym@mail.ru>
2018-03-13 23:12:02 +0300

    Some minor changes

--- src/main/java/com/infosec/accessanalysis/dal/repository/DepartmentRepository.java
+++ src/main/java/com/infosec/accessanalysis/dal/repository/DepartmentRepository.java
@@ -2,7 +2,5 @@ package com.infosec.accessanalysis.dal.repository;
 
 import com.infosec.accessanalysis.dal.model.Department;
 
-import java.util.List;
-
 public interface DepartmentRepository extends HierarchicalRepository<Department> {
 }
--- src/main/java/com/infosec/accessanalysis/dal/repository/DepartmentRepositoryMSSQL.java
+++ src/main/java/com/infosec/accessanalysis/dal/repository/DepartmentRepositoryMSSQL.java
@@ -13,19 +13,20 @@ import java.util.List;
 public class DepartmentRepositoryMSSQL implements DepartmentRepository {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private String url = Configuration.getDbUrl();
+    private String dbUrl = Configuration.getDbUrl();
 
     @Override
     public List<Department> findAll() throws SQLException {
         List<Department> departments = new LinkedList<>();
 
         try (
-                Connection conn = DriverManager.getConnection(url);
+                Connection conn = DriverManager.getConnection(dbUrl);
                 Statement st = conn.createStatement();
                 ResultSet rs = st.executeQuery("SELECT DEPARTMENT_ID, PARENT_ID, NAME FROM dbo.DEPARTMENT WHERE IS_DELETED = 0 ORDER BY DEPARTMENT_ID");
         ) {
             while (rs.next()) {
-                Department dep = new Department(rs.getInt(1),
+                Department dep = new Department(
+                        rs.getInt(1),
                         rs.getInt(2),
                         rs.getString(3));
                 loadChildObjects(dep);
@@ -41,14 +42,15 @@ public class DepartmentRepositoryMSSQL implements DepartmentRepository {
         List<Department> departments = new LinkedList<>();
 
         try (
-                Connection conn = DriverManager.getConnection(url);
+                Connection conn = DriverManager.getConnection(dbUrl);
                 PreparedStatement st = conn.prepareStatement(";WITH DEP AS (SELECT DEPARTMENT_ID, PARENT_ID, NAME, ROW_NUMBER() OVER (ORDER BY DEPARTMENT_ID) as ROW_NUM FROM dbo.DEPARTMENT WHERE IS_DELETED = 0) SELECT * FROM DEP WHERE ROW_NUM >= ? AND ROW_NUM < ?");
         ) {
             st.setLong(1, from + 1);
             st.setLong(2, from + 1 + count);
             try (ResultSet rs = st.executeQuery()) {
                 while (rs.next()) {
-                    Department dep = new Department(rs.getInt(1),
+                    Department dep = new Department(
+                            rs.getInt(1),
                             rs.getInt(2),
                             rs.getString(3));
                     loadChildObjects(dep);
@@ -69,13 +71,14 @@ public class DepartmentRepositoryMSSQL implements DepartmentRepository {
         List<Department> departments = new LinkedList<>();
 
         try (
-                Connection conn = DriverManager.getConnection(url);
+                Connection conn = DriverManager.getConnection(dbUrl);
                 PreparedStatement st = conn.prepareStatement("SELECT DEPARTMENT_ID, PARENT_ID, NAME FROM dbo.DEPARTMENT WHERE PARENT_ID = ? AND IS_DELETED = 0");
         ) {
             st.setLong(1, id);
             try (ResultSet rs = st.executeQuery()) {
                 while (rs.next()) {
-                    Department dep = new Department(rs.getInt(1),
+                    Department dep = new Department(
+                            rs.getInt(1),
                             rs.getInt(2),
                             rs.getString(3));
                     loadChildObjects(dep);
@@ -90,13 +93,14 @@ public class DepartmentRepositoryMSSQL implements DepartmentRepository {
     @Override
     public Department findParent(long id) throws SQLException {
         try (
-                Connection conn = DriverManager.getConnection(url);
+                Connection conn = DriverManager.getConnection(dbUrl);
                 PreparedStatement st = conn.prepareStatement("SELECT DEPARTMENT_ID, PARENT_ID, NAME FROM dbo.DEPARTMENT WHERE PARENT_ID = ? AND IS_DELETED = 0");
         ) {
             st.setLong(1, id);
             try (ResultSet rs = st.executeQuery()) {
                 if (rs.next()) {
-                    Department dep = new Department(rs.getInt(1),
+                    Department dep = new Department(
+                            rs.getInt(1),
                             rs.getInt(2),
                             rs.getString(3));
                     loadChildObjects(dep);
@@ -113,12 +117,13 @@ public class DepartmentRepositoryMSSQL implements DepartmentRepository {
         List<Department> departments = new LinkedList<>();
 
         try (
-                Connection conn = DriverManager.getConnection(url);
+                Connection conn = DriverManager.getConnection(dbUrl);
                 Statement st = conn.createStatement();
                 ResultSet rs = st.executeQuery("SELECT DEPARTMENT_ID, PARENT_ID, NAME FROM dbo.DEPARTMENT WHERE PARENT_ID IS NULL AND IS_DELETED = 0");
         ) {
             while (rs.next()) {
-                Department dep = new Department(rs.getInt(1),
+                Department dep = new Department(
+                        rs.getInt(1),
                         rs.getInt(2),
                         rs.getString(3));
                 loadChildObjects(dep);
@@ -132,13 +137,14 @@ public class DepartmentRepositoryMSSQL implements DepartmentRepository {
     @Override
     public Department findOne(long id) throws SQLException {
         try (
-                Connection conn = DriverManager.getConnection(url);
+                Connection conn = DriverManager.getConnection(dbUrl);
                 PreparedStatement st = conn.prepareStatement("SELECT DEPARTMENT_ID, PARENT_ID, NAME FROM dbo.DEPARTMENT WHERE DEPARTMENT_ID = ? AND IS_DELETED = 0");
         ) {
             st.setLong(1, id);
             try (ResultSet rs = st.executeQuery()) {
                 if (rs.next()) {
-                    Department dep = new Department(rs.getInt(1),
+                    Department dep = new Department(
+                            rs.getInt(1),
                             rs.getInt(2),
                             rs.getString(3));
                     loadChildObjects(dep);
@@ -152,7 +158,7 @@ public class DepartmentRepositoryMSSQL implements DepartmentRepository {
 
     private void loadChildObjects(Department department) throws SQLException {
         try (
-                Connection conn = DriverManager.getConnection(url);
+                Connection conn = DriverManager.getConnection(dbUrl);
                 PreparedStatement st = conn.prepareStatement("SELECT\n" +
                         "\tpers.PERSONAGE_ID\n" +
                         "\t, emp.FIRST_NAME\n" +
@@ -179,7 +185,8 @@ public class DepartmentRepositoryMSSQL implements DepartmentRepository {
             try (ResultSet rs = st.executeQuery()) {
                 List<Personage> employees = department.getEmployees();
                 while (rs.next()) {
-                    Personage personage = new Personage(rs.getInt(1),
+                    Personage personage = new Personage(
+                            rs.getInt(1),
                             rs.getString(2),
                             rs.getString(3),
                             rs.getString(4),
